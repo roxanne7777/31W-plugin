e770db4313ddd062f536f800c3d6dfb00ab46bd1
roxanne7777 <0533864@cmontmorency.qc.ca>
2024-11-15 21:56:26 -0500

    tp1 ajouter boutons svg

--- carrousel.php
+++ carrousel.php
@@ -37,9 +37,9 @@ function generer_carrousel(){
     $chaine = '
     <button class="carrousel__bouton" id="carrousel__bouton">Ouvrir</button>
     <div class="carrousel">
-        <button class="carrousel__x">X</button>
-        <button class="carrousel__gauche">gauche</button>
-        <button class="carrousel__droite">droite</button>
+        <button class="carrousel__x"><img src="https://s2.svgbox.net/hero-solid.svg?ic=x&color=000" width="32" height="32"></button>
+        <button class="carrousel__gauche"><img src="https://s2.svgbox.net/hero-solid.svg?ic=chevron-left&color=000" width="32" height="32"></button>
+        <button class="carrousel__droite"><img src="https://s2.svgbox.net/hero-solid.svg?ic=chevron-right&color=000" width="32" height="32"></button>
         <figure class="carrousel__figure"></figure>
         <form class="carrousel__form"></form>
     </div>';
