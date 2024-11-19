cd3525270d6a4c5bd1eb9d914237c64e2b8d1c03
roxanne7777 <112108013+roxanne7777@users.noreply.github.com>
2024-11-19 00:30:15 -0500

    ajouter fonction charger_scripts_css

--- filtrepost/filtrepost.php
+++ filtrepost/filtrepost.php
@@ -5,14 +5,24 @@ author: Roxanne Auclair
 description: Une extension qui permettra de filtrer nos articles
 */ 
 
-function enfiler_css_js() {
-    filemtime(plugin_dir_path(__FILE__))
-    filemtime(plugin_dir_path(__FILE__))
-    plugin_dir_url(__FILE__)
-    wp_enqueue_style();
-    wp_enqueue_script()
-    
+function charger_scripts_css(){
+ 
+    $version_css = filemtime(plugin_dir_path(__FILE__). "/style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/filtrepost.js");
+ 
+    wp_enqueue_style(
+        "filtrepost",        
+        plugin_dir_url(__FILE__) . "/style.css",
+        array(),
+        $version_css
+    ) ;  
+ 
+    wp_enqueue_script(
+        "filtrepost",      
+        plugin_dir_url(__FILE__) . "/js/filtrepost.js",
+        array(),
+        $version_js,
+        true
+    )  ;
 }
 add_action('wp_enqueue_scripts', 'charger_scripts_css');
-
-?>
